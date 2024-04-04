34356fc4fea0a5a0a3dbc8ffb4dd4ab8719809b8
Sirjan baral <sirjan.gnr@gmail.com>
2024-04-04 15:47:56 +0545

    fix: fetch url get request changed to post

--- app/routes/fetchurl.py
+++ app/routes/fetchurl.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, HTTPException, Depends
-
-from typing import Annotated
+from fastapi import APIRouter,  Depends,Request
+from pydantic import BaseModel
 
 from http import HTTPStatus
 
@@ -9,18 +8,22 @@ from app.services import get_user
 
 router  = APIRouter()
 
-@router.get("/api/v1/geturl/", tags=["urls"], status_code=HTTPStatus.OK)
+class URLRequest(BaseModel):
+    base_url: str
+
+@router.post("/api/v1/fetchurl/", tags=["urls"], status_code=HTTPStatus.OK)
 def fetch_url(
-    base_url:str,
-    current_user: Annotated[str, Depends(get_user.get_current_user)]
-    ):
+    request: Request,
+    url_request: URLRequest,
+    # current_user: str = Depends(get_user.get_current_user)
+):
     """Fetch all the anchor tag from the url"""
 
     ##TODO: Check for the number of urls, for premium
+    base_url = url_request.base_url
     tai_scraper = scrape_url.ScrapeWebPage(base_url)
     url_list, base_url = tai_scraper.get_url()
     processed_url = tai_scraper.process_urls(url_list=url_list, base_url=base_url)
     # content = tai_scraper.get_page_contents(url_list = set(processed_url))
-    response = {"success":"true",
-                "urls": processed_url}
+    response = {"success": "true", "urls": processed_url}
     return response
